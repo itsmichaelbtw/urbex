1193f67d9fe230fb038048170b23072bff90ba28
Michael <me@miikey.pw>
2022-10-11 13:50:41 +1100

    test: migrated from `jest` to `mocha` and `karma` to utilize browser based tests

---
+++ karma.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from "karma";
+
+const testGlob = "test/**/*.test.ts";
+const libGlobal = "lib/**/*.ts";
+
+export default (config: Config) => {
+    config.set({
+        basePath: "",
+        frameworks: ["mocha", "chai", "karma-typescript"],
+        files: [
+            {
+                pattern: libGlobal
+            },
+            {
+                pattern: testGlob
+            }
+        ],
+        plugins: [
+            "karma-mocha",
+            "karma-chai",
+            "karma-chrome-launcher",
+            "karma-firefox-launcher",
+            "karma-typescript"
+        ],
+        preprocessors: {
+            [libGlobal]: ["karma-typescript"],
+            [testGlob]: ["karma-typescript"]
+        },
+        reporters: ["progress", "karma-typescript"],
+        browsers: ["ChromeHeadless", "FirefoxHeadless"],
+        port: 9876,
+        captureTimeout: 4 * 60 * 1000,
+        browserDisconnectTimeout: 10000,
+        browserDisconnectTolerance: 1,
+        browserNoActivityTimeout: 4 * 60 * 1000,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        // @ts-ignore
+        karmaTypescriptConfig: {
+            compilerOptions: {
+                lib: ["ES2015", "DOM", "DOM.Iterable"],
+                esModuleInterop: true,
+                module: "commonjs",
+                target: "ES2015"
+            },
+            include: [testGlob, libGlobal]
+        }
+    });
+};
