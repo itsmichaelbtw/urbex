8413a53fa59eacf64d2ed3277f42d4a4bf7af362
Michael <me@miikey.pw>
2022-10-11 13:43:11 +1100

    chore: project utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,20 @@ export function hasOwnProperty<X extends {}, Y extends PropertyKey>(
 export function isUndefined(value: unknown): value is undefined {
     return typeof value === "undefined";
 }
+
+export function isNegative(value: number): boolean {
+    return value < 0;
+}
+
+export function isPositive(value: number): boolean {
+    return value > 0;
+}
+
+export function absolute(value: number): number {
+    return Math.abs(value);
+}
+
+export function round(value: number, precision: number): number {
+    const multiplier = Math.pow(10, precision);
+    return Math.round(value * multiplier) / multiplier;
+}
