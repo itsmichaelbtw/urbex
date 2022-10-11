889ac1c2c4c9f1de3007bfefa6997956b0c0e0ce
Michael <me@miikey.pw>
2022-10-11 13:48:22 +1100

    feat(cache): new ttl-cache to store items with a ttl

---
+++ lib/ttl-cache/ttl-cache.ts
@@ -0,0 +1,141 @@
+import { environment } from "../urbex";
+import { isUndefined, isNegative, absolute } from "../utils";
+import { timeProvider } from "./time-provider";
+import { CacheClock } from "./cache-clock";
+import { hash } from "./hash";
+
+type CacheMap = Map<string, CacheItem>;
+type CacheTTL = number;
+
+interface CacheItem {
+    value: any;
+    expires: number;
+}
+
+interface CacheOptions {
+    maxItems: number;
+    ttl: CacheTTL;
+}
+
+interface CacheManipulatorOptions {
+    ttl: CacheTTL;
+}
+
+const defaultCacheOptions: CacheOptions = {
+    maxItems: 1000,
+    ttl: Infinity
+};
+
+function assignOptions<P, T>(defaultOptions: P, options: T): P & T {
+    return Object.assign({}, defaultOptions, options);
+}
+
+function parseOptions(
+    options: Partial<CacheOptions> = {},
+    defaultOptions: CacheOptions
+): CacheOptions {
+    const opts = assignOptions(defaultOptions, options);
+
+    if (isNegative(opts.maxItems)) {
+        opts.maxItems = absolute(opts.maxItems);
+    }
+
+    if (isNegative(opts.ttl)) {
+        opts.ttl = absolute(opts.ttl);
+    }
+
+    if (opts.maxItems === 0) {
+        opts.maxItems = 1;
+    }
+
+    return opts;
+}
+
+export class TTLCache {
+    private _options: CacheOptions;
+    private _cache: CacheMap = new Map();
+    private _cacheClock: CacheClock = new CacheClock();
+
+    constructor(options?: Partial<CacheOptions>) {
+        const opts = parseOptions(options, defaultCacheOptions);
+
+        this._options = opts;
+        this._cacheClock.configure({
+            ttl: opts.ttl,
+            onExpire: (key: string) => {
+                this.remove(key);
+            }
+        });
+    }
+
+    static create(options?: Partial<CacheOptions>): TTLCache {
+        return new TTLCache(options);
+    }
+
+    get options(): CacheOptions {
+        return this._options;
+    }
+
+    get size(): number {
+        return this._cache.size;
+    }
+
+    set(key: string, value: any, options?: CacheManipulatorOptions): void {
+        const keyHash = hash(key);
+
+        if (this._cache.has(keyHash)) {
+            this._cacheClock.remove(keyHash);
+        }
+
+        const { ttl } = parseOptions(options, this.options);
+
+        const item: CacheItem = {
+            value: value,
+            expires: timeProvider.now() + ttl
+        };
+
+        this._cache.set(keyHash, item);
+        this._cacheClock.add(keyHash, ttl);
+    }
+
+    get(key: string, isHashed: boolean = false): any {
+        if (!isHashed) {
+            key = hash(key);
+        }
+
+        const item = this._cache.get(key);
+
+        if (isUndefined(item)) {
+            return undefined;
+        }
+
+        return item.value;
+    }
+
+    has(key: string): boolean {
+        return this._cache.has(key);
+    }
+
+    remove(key: string): void {
+        this._cache.delete(key);
+    }
+
+    prune(): void {
+        const now = timeProvider.now();
+
+        for (const [key, item] of this._cache) {
+            if (item.expires < now) {
+                this.remove(key);
+            }
+        }
+    }
+
+    clear(): void {
+        this._cache.clear();
+        this._cacheClock.clear();
+    }
+
+    [Symbol.iterator](): IterableIterator<[string, CacheItem]> {
+        return this._cache.entries();
+    }
+}
