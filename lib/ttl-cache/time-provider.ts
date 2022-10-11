80a758efc12667aef7271a53bd9ddc3342dcdab8
Michael <me@miikey.pw>
2022-10-11 13:47:08 +1100

    feat(cache): find a suitable time provider based on env context

---
+++ lib/ttl-cache/time-provider.ts
@@ -0,0 +1,25 @@
+import { environment } from "../urbex";
+
+interface TimeProvider {
+    now(): number;
+}
+
+function getBestTimeProvider(): TimeProvider {
+    try {
+        // if (environment.isNode) {
+        //     return require("perf_hooks").performance;
+        // }
+
+        // if (environment.isBrowser) {
+        //     if (window.performance && window.performance.now) {
+        //         return window.performance;
+        //     }
+        // }
+
+        return Date;
+    } catch (error) {
+        return Date;
+    }
+}
+
+export const timeProvider = getBestTimeProvider();
