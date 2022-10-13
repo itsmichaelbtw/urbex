a9eeff8d26fb38fe664a7d2866fb9444e249baaf
Michael <me@miikey.pw>
2022-10-13 20:29:56 +1100

    feat(utils): extra utility methods

--- lib/utils.ts
+++ lib/utils.ts
@@ -25,3 +25,47 @@ export function round(value: number, precision: number): number {
     const multiplier = Math.pow(10, precision);
     return Math.round(value * multiplier) / multiplier;
 }
+
+export function isArray<T>(value: unknown): value is T[] {
+    return Array.isArray(value);
+}
+
+export function isObject(value: unknown): value is Record<string, unknown> {
+    return typeof value === "object" && value !== null;
+}
+
+export function isEmpty(value: any): boolean {
+    if (isArray(value)) {
+        return value.length === 0;
+    } else if (isObject(value)) {
+        return Object.keys(value).length === 0;
+    } else {
+        return !value;
+    }
+}
+
+export function capitalize(value: string): string {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
+export function uppercase(value: string): string {
+    return value.toUpperCase();
+}
+
+export function lowercase(value: string): string {
+    return value.toLowerCase();
+}
+
+export function clone<T>(value: T): T {
+    if (isArray(value)) {
+        return value.slice() as T;
+    } else if (isObject(value)) {
+        return { ...value } as T;
+    } else {
+        return value;
+    }
+}
+
+export function assignOptions<P, T>(defaultOptions: P, options: T): P & T {
+    return Object.assign({}, defaultOptions, options);
+}
