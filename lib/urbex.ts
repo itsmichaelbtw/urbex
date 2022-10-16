40288e0adb62963871f3ca04855c3136b286b2f7
Michael <me@miikey.pw>
2022-10-16 23:15:16 +1100

    feat(urbex): added default export to the extended urbex client
    extended client includes extra methods not available on the class

--- lib/urbex.ts
+++ lib/urbex.ts
@@ -1,3 +1,47 @@
-import { Environment, env } from "./environment";
+import { environment, Environment, env } from "./environment";
+import { UrbexClient, isUrbexClient } from "./core/urbex";
 
-export const environment = new Environment();
+interface IUrbexClient extends UrbexClient {
+    /**
+     * Create a new isolated instance of the Urbex client
+     *
+     * Any existing configuration will be copied to the new
+     * instance. Furthermore, changes made to the new instance
+     * will not affect the original instance
+     */
+    isolateClient(): UrbexClient;
+    /**
+     *
+     * TypeScript safe guard to check if an object is an instance of UrbexClient
+     */
+    isUrbexClient(client: unknown): client is UrbexClient;
+    /**
+     * The underlying UrbexClient class which can be used to create new instances
+     *
+     * Recommended to use `isolateClient` instead
+     */
+    Client: typeof UrbexClient;
+    /**
+     * The current environment of the project
+     */
+    environment: Environment;
+}
+
+function createClient(): IUrbexClient {
+    const client = UrbexClient.create();
+    const extendedClient = client as IUrbexClient;
+
+    extendedClient.isolateClient = UrbexClient.create;
+    extendedClient.environment = environment;
+    extendedClient.isUrbexClient = isUrbexClient;
+    extendedClient.Client = UrbexClient;
+
+    return extendedClient;
+}
+
+const urbex = createClient();
+
+export { environment, env };
+export default urbex;
+
+export * from "./core/headers";
