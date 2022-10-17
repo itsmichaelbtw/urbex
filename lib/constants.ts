8331bb24f3a0c7f3686b258c4f93561f4b7d3fd7
Michael <me@miikey.pw>
2022-10-17 21:03:16 +1100

    chore(constants): added regexp constants and methods array

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,19 @@
+import type { Methods } from "./types";
+
 export const DOTENV_FILENAME = ".env";
 export const DOTENV_LINE = new RegExp(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/);
+
+export const PROTOCOL_REGEXP = new RegExp(/^(https|http)/i);
+export const HOSTNAME_REGEXP = new RegExp(
+    /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/i
+);
+
+export const METHODS: Methods[] = [
+    "PUT",
+    "POST",
+    "PATCH",
+    "OPTIONS",
+    "HEAD",
+    "GET",
+    "DELETE"
+];
