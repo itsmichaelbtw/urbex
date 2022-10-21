4686c1c1f8b9b174b48da5a9f92bdc52e87b19ff
Michael <me@miikey.pw>
2022-10-21 11:47:58 +1100

    chore: added url regex patterns

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,8 +5,16 @@ export const DOTENV_LINE = new RegExp(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/);
 
 export const PROTOCOL_REGEXP = new RegExp(/^(https|http)/i);
 export const HOSTNAME_REGEXP = new RegExp(
-    /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/i
+    /^(?:https?:\/\/)?(?:[^@\/\n]+@)?([^:\/\n]+)/i
 );
+export const PORT_REGEXP = new RegExp(/:(\d{2,5})$/);
+
+export const URL_REGEXP = new RegExp(
+    /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
+);
+
+export const URI_TEMPLATE_REGEXP_LEFT = "[{][^{{]*\\b";
+export const URI_TEMPLATE_REGEXP_RIGHT = "\\b[^{}]*[}]";
 
 export const METHODS: Methods[] = [
     "PUT",
