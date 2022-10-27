85fa778f3aa35391747a0d18e1ef6a5d3464a936
Michael <me@miikey.pw>
2022-10-27 11:34:35 +1100

    chore: cleaned up request api methods (wip)

--- lib/core/api/request-api.ts
+++ lib/core/api/request-api.ts
@@ -1,16 +1,26 @@
 import type { UrbexContext } from "../../environment/environment";
 import type {
-    UrbexRequestApi,
     DispatchedResponse,
-    UrbexClientOptions,
-    InternalUrbexConfiguration
+    ParsedClientConfiguration,
+    UrbexRequestApi
 } from "../types";
 
 import { NodeRequest } from "./http";
 import { BrowserRequest } from "./xhr";
 import { environment } from "../../environment";
-import { createPromise } from "../../utils";
-import { convertURIComponentToString, URLStringBuilder } from "../url";
+import { UrbexHeaders } from "../headers";
+import {
+    createPromise,
+    replaceObjectProperty,
+    isEmpty,
+    isObject,
+    merge
+} from "../../utils";
+import { convertURIComponentToString } from "../url";
+
+// here all of the interceptors are checked
+// cache clocks are checked here
+// the response is created here
 
 export class RequestApi {
     protected $api: UrbexRequestApi = null;
@@ -21,12 +31,12 @@ export class RequestApi {
 
     private register(context: UrbexContext) {
         if (context === "browser" && typeof XMLHttpRequest !== "undefined") {
-            // this.$api = new BrowserRequest();
+            this.$api = new BrowserRequest();
             return;
         }
 
         if (context === "node") {
-            // this.$api = new NodeRequest();
+            this.$api = new NodeRequest();
             return;
         }
 
@@ -35,21 +45,9 @@ export class RequestApi {
         );
     }
 
-    // this intakes an internal config object
-    protected dispatchRequest(
-        config: InternalUrbexConfiguration
+    protected async dispatchRequest(
+        config: ParsedClientConfiguration
     ): DispatchedResponse {
-        // only thing we have to do here is build the url
-        // this shouldn't be responsible for validating the config object
-        // will need to check the cache, not sure whether to perform
-        // this op here or before this method is called
-
-        const targetURL = config.url.href;
-
-        console.log(config);
-
-        return createPromise((resolve, reject) => {
-            // this.$api.send().then(resolve).catch(reject)
-        });
+        return this.$api.send(config);
     }
 }
