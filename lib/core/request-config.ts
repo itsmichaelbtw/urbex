93228ce00e43cf7e79be938a9c765ae85cbe148d
Michael <me@miikey.pw>
2022-11-18 13:12:10 +1100

    fix(uriParser): failing to correctly update the `url.href` #4

--- lib/core/request-config.ts
+++ lib/core/request-config.ts
@@ -5,8 +5,10 @@ import type {
     ParsedClientConfiguration,
     ConfigurableUrbexClient,
     ParsableRequestConfig,
-    SafeParsedClientConfiguration
+    SafeParsedClientConfiguration,
+    ConfigurableClientUrl
 } from "./types";
+import type { URIParserModifier } from "./url";
 
 import { UrbexHeaders } from "./headers";
 import { environment } from "../environment";
@@ -23,7 +25,7 @@ import {
     argumentIsNotProvided,
     isEmpty
 } from "../utils";
-import { isValidURL, serializeParams, uriParser } from "./url";
+import { isValidURL, serializeParams, parseURIIntoComponent } from "./url";
 import { PROTOCOL_REGEXP, HOSTNAME_REGEXP, METHODS } from "../constants";
 import { DEFAULT_CLIENT_OPTIONS } from "./constants";
 
@@ -33,7 +35,8 @@ function determineAppropriateURI() {
         hostname: null,
         urlMount: "/api",
         endpoint: null,
-        port: null
+        port: null,
+        params: null
     };
 
     if (environment.isBrowser) {
@@ -55,19 +58,11 @@ function determineAppropriateURI() {
     return uriOptions;
 }
 
-function validateUriComponent(uri: URIComponent) {
-    if (argumentIsNotProvided(uri)) {
-        throw new Error("URI component is not provided.");
-    }
-
-    return uriParser(clone(uri));
-}
-
 export class RequestConfig {
     private $config: ParsedClientConfiguration;
 
     constructor(config?: ConfigurableUrbexClient) {
-        const uriComponent = uriParser(determineAppropriateURI());
+        const uriComponent = parseURIIntoComponent(determineAppropriateURI());
 
         this.$config = merge(DEFAULT_CLIENT_OPTIONS, {
             url: uriComponent,
@@ -75,23 +70,29 @@ export class RequestConfig {
         });
 
         if (isObject(config) && !isEmpty(config)) {
-            this.set(config);
+            this.set(this.createConfigurationObject(config, true));
         }
+
+        this.$config.pipelines.request.unshift((config) => {
+            return Promise.resolve(config);
+        });
     }
 
     public defaultConfig(): ParsedClientConfiguration {
         return merge(DEFAULT_CLIENT_OPTIONS, {
-            url: uriParser(determineAppropriateURI()),
+            url: parseURIIntoComponent(determineAppropriateURI()),
             headers: new UrbexHeaders()
         });
     }
 
-    public validateURIComponent(uri: URIComponent): URIComponent {
-        if (argumentIsNotProvided(uri)) {
-            throw new Error("URI component is not provided.");
-        }
+    public createConfigurationObject(
+        config: ConfigurableUrbexClient,
+        allowEndpoints: boolean
+    ): ParsedClientConfiguration {
+        const parsed = this.parseIncomingConfig(config, allowEndpoints);
+        const merged = this.merge(parsed);
 
-        return validateUriComponent(uri);
+        return merged;
     }
 
     public parseIncomingConfig<T extends ConfigurableUrbexClient>(
@@ -99,41 +100,35 @@ export class RequestConfig {
         allowEndpoints: boolean
     ): ParsableRequestConfig {
         if (argumentIsNotProvided(config) || !isObject(config)) {
-            throw new Error(
-                "The configuration must be an object with valid properties."
-            );
+            throw new Error("The configuration must be an object with valid properties.");
         }
 
         const configuration = clone(config);
 
-        if (hasOwnProperty(configuration, "params")) {
-            configuration.params = serializeParams(
-                configuration.params,
-                "object"
-            );
-        }
-
         if (hasOwnProperty(configuration, "url")) {
+            const currentUrlConfig = this.get().url;
+
             if (isObject(configuration.url)) {
                 // have to merge otherwise the uri parser may
                 // throw an error if fewer values are provided
-                configuration.url = merge(this.get().url, configuration.url);
+                configuration.url = merge(currentUrlConfig, configuration.url);
             }
 
-            configuration.url = uriParser(
-                configuration.url,
-                configuration.params,
-                allowEndpoints
-            );
+            if (allowEndpoints && configuration.url.toString().startsWith("/")) {
+                configuration.url = merge(currentUrlConfig, {
+                    endpoint: configuration.url
+                });
+            }
+
+            const parsed = parseURIIntoComponent(configuration.url, allowEndpoints);
+            configuration.url = parsed;
         }
 
         if (hasOwnProperty(configuration, "method")) {
             const method = uppercase(configuration.method);
 
             if (!METHODS.includes(method)) {
-                throw new Error(
-                    `The method ${method} is not a valid HTTP method.`
-                );
+                throw new Error(`The method ${method} is not a valid HTTP method.`);
             }
 
             configuration.method = method;
@@ -147,38 +142,9 @@ export class RequestConfig {
         });
     }
 
-    public set<T extends ConfigurableUrbexClient>(
-        config: T,
-        allowEndpoints: boolean = false
-    ): ParsedClientConfiguration {
-        const configuration = this.parseIncomingConfig(config, allowEndpoints);
-        const merged = this.merge(configuration);
-
-        this.$config = merged;
-
-        return merged;
-    }
-
-    /**
-     * Convert a parsed configuration into a safe configuration.
-     * This removes any properties that are not safe to expose.
-     *
-     * Returns a JSON object.
-     */
-    public toJSON(
-        config?: ParsedClientConfiguration
-    ): SafeParsedClientConfiguration {
-        if (argumentIsNotProvided(config) || !isObject(config)) {
-            return this.get();
-        }
-
-        const headers = config.headers.get();
-
-        return merge(config, { headers });
-    }
-
-    public get(): SafeParsedClientConfiguration {
-        return this.toJSON(this.$config);
+    public set(config: ParsedClientConfiguration): ParsedClientConfiguration {
+        this.$config = config;
+        return config;
     }
 
     public merge(config?: ParsableRequestConfig): ParsedClientConfiguration {
@@ -197,6 +163,26 @@ export class RequestConfig {
         const headersObject = UrbexHeaders.construct(mergedHeaders);
 
         return merge(merged, { headers: headersObject });
+    }
+
+    public get(): SafeParsedClientConfiguration {
+        return this.toJSON(this.$config);
+    }
+
+    /**
+     * Convert a parsed configuration into a safe configuration.
+     * This removes any properties that are not safe to expose.
+     *
+     * Returns a JSON object.
+     */
+    public toJSON(config?: ParsedClientConfiguration): SafeParsedClientConfiguration {
+        if (argumentIsNotProvided(config) || !isObject(config)) {
+            return this.get();
+        }
+
+        const headers = config.headers.get();
+
+        return merge(config, { headers });
     }
 
     /**
