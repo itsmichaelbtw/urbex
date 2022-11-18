0f94478f77e79073a3f6fa4172614d43a20bb7f1
Michael <me@miikey.pw>
2022-11-18 13:12:33 +1100

    refactor: removed `new RegExp` encasing

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,13 +3,10 @@ import type { Methods } from "./types";
 // support for any protocols to be used with the uri component
 // currently only http and https are supported
 
-// passing a string bypasses the protocol check and allows for any protocol to be used
-// but when using the object, url.protocol only accepts http and https
-export const PROTOCOL_REGEXP = new RegExp(/^(https|http)/i);
-export const HOSTNAME_REGEXP = new RegExp(
-    /^(?:https?:\/\/)?(?:[^@\/\n]+@)?([^:\/\n]+)/i
-);
-export const PORT_REGEXP = new RegExp(/:(\d{2,5})$/);
+// regex to get the protocol from the uri component, can match anything
+export const PROTOCOL_REGEXP = /^([a-z0-9]+):\/\//i;
+export const HOSTNAME_REGEXP = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?([^:\/\n]+)/i;
+export const PORT_REGEXP = /:(\d{2,5})$/;
 
 export const URL_REGEXP = new RegExp(
     /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
@@ -18,12 +15,4 @@ export const URL_REGEXP = new RegExp(
 export const URI_TEMPLATE_REGEXP_LEFT = "[{][^{{]*\\b";
 export const URI_TEMPLATE_REGEXP_RIGHT = "\\b[^{}]*[}]";
 
-export const METHODS: Methods[] = [
-    "PUT",
-    "POST",
-    "PATCH",
-    "OPTIONS",
-    "HEAD",
-    "GET",
-    "DELETE"
-];
+export const METHODS: Methods[] = ["PUT", "POST", "PATCH", "OPTIONS", "HEAD", "GET", "DELETE"];
