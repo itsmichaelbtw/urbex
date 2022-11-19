181297630935c56bfe8addaa89fd5b612df7bb22
Michael <me@miikey.pw>
2022-11-19 17:46:50 +1100

    chore: removed explicit type on methods array

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-import type { Methods } from "./types";
+import type { MethodsUpper } from "./types";
 
 // support for any protocols to be used with the uri component
 // currently only http and https are supported
@@ -14,4 +14,4 @@ export const URL_REGEXP =
 export const URI_TEMPLATE_REGEXP_LEFT = "[{][^{{]*\\b";
 export const URI_TEMPLATE_REGEXP_RIGHT = "\\b[^{}]*[}]";
 
-export const METHODS: Methods[] = ["PUT", "POST", "PATCH", "OPTIONS", "HEAD", "GET", "DELETE"];
+export const METHODS = ["PUT", "POST", "PATCH", "OPTIONS", "HEAD", "GET", "DELETE"];
