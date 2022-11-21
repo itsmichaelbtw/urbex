4a3180276879cedc9dc9b17a86a70d044cf6f972
Michael <me@miikey.pw>
2022-11-21 17:06:28 +1100

    chore: added empty `DECODERS` export for merged support with `http.js`

--- lib/core/api/xhr.ts
+++ lib/core/api/xhr.ts
@@ -1,8 +1,12 @@
 import type { InternalConfiguration } from "../../exportable-types";
 import type { DispatchedResponse, UrbexRequestApi, DispatchedAPIRequest } from "../../types";
 
+import { createEmptyScheme } from "../../utils";
+
 export class BrowserRequest implements UrbexRequestApi {
     public send(config: InternalConfiguration): DispatchedAPIRequest {
         return new Promise(() => {});
     }
 }
+
+export const DECODERS = createEmptyScheme(["br", "gzip", "deflate", "compress"]);
