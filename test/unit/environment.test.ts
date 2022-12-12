a437d92380197070ab9ce5763d0687244aa77c85
Michael <me@miikey.pw>
2022-12-12 22:02:14 +1100

    test: unit test for the new url parser class

--- test/unit/environment.test.ts
+++ test/unit/environment.test.ts
@@ -2,14 +2,21 @@ import chai from "chai";
 import envAgent from "env-agent";
 
 import { Environment } from "../../lib/environment";
+import { URLParser } from "../../lib/core/parsers/url-parser";
 
 const environment = new Environment();
 
 envAgent.options.overwrite = true;
 
-describe("new Environment", () => {
+describe("environment", () => {
     before(() => {
         envAgent.set("NODE_ENV", "development");
+    });
+
+    it("should create a new environment instance", () => {
+        const env = new Environment();
+
+        chai.assert.instanceOf(env, Environment);
     });
 
     it("should detect the environment context (browser)", function () {
@@ -50,5 +57,36 @@ describe("new Environment", () => {
 
         chai.assert.isTrue(environment.isProduction);
         chai.assert.isFalse(environment.isDevelopment);
+    });
+
+    it("should create the correct environment component (browser)", function () {
+        if (environment.isNode) {
+            this.skip();
+        }
+    });
+
+    it("should create the correct environment component (node)", function () {
+        if (environment.isBrowser) {
+            this.skip();
+        }
+
+        const parser = environment.getEnvironmentComponent();
+
+        chai.assert.instanceOf(parser, URLParser);
+
+        const envComponent = parser.toJSON();
+
+        chai.expect(envComponent).to.have.property("protocol");
+        chai.expect(envComponent).to.have.property("hostname");
+        chai.expect(envComponent).to.have.property("port");
+        chai.expect(envComponent.protocol).to.equal("http");
+        chai.expect(envComponent.hostname).to.equal("localhost");
+        chai.expect(envComponent.port).to.equal(3000);
+
+        envAgent.set("PORT", "8080");
+
+        const parser2 = environment.getEnvironmentComponent();
+
+        chai.expect(parser2.toJSON().port).to.equal(8080);
     });
 });
