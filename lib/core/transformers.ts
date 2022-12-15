19523dd816e61fd64e788e5ba12c0cbff0dbdb19
Michael <me@miikey.pw>
2022-12-15 23:27:02 +1100

    feat: request body is now properly parsed and transformed

--- lib/core/transformers.ts
+++ lib/core/transformers.ts
@@ -4,15 +4,86 @@ import { REQUEST_BODY_METHODS } from "./constants";
 import { PipelineExecutor } from "./pipelines";
 import { environment } from "../environment";
 import { DECODERS } from "./api/http";
-import { safeJSONParse, uppercase } from "../utils";
+import {
+    isNil,
+    isObject,
+    isArray,
+    isArrayBuffer,
+    isArrayBufferView,
+    isString,
+    isBuffer,
+    isFile,
+    isBlob,
+    isStream,
+    isFunction,
+    safeJSONParse,
+    safeStringify,
+    uppercase,
+    ensureContentLength,
+    convertToEncodedForm,
+    isObjectLike
+} from "../utils";
 
 const SKIPPABLE_RESPONSE_TYPES = ["stream", "raw"];
 
+function toBuffer(value: any): Buffer | Blob {
+    if (environment.isNode) {
+        return Buffer.from(value);
+    }
+
+    return new Blob([value]);
+}
+
 export const transformRequestData = new PipelineExecutor<RequestExecutor>((config) => {
-    if (REQUEST_BODY_METHODS.includes(uppercase(config.method))) {
+    if (!REQUEST_BODY_METHODS.includes(uppercase(config.method))) {
+        config.data = undefined;
+        config.headers.delete("Content-Type");
+    }
+
+    if (
+        isNil(config.data) ||
+        isBuffer(config.data) ||
+        isFile(config.data) ||
+        isBlob(config.data) ||
+        isStream(config.data)
+    ) {
+        return Promise.resolve(config);
+    }
+
+    if (isArrayBuffer(config.data) || isArrayBufferView(config.data)) {
+        config.data = toBuffer(config.data);
+
+        ensureContentLength.call(config.headers, config.data.byteLength);
+        return Promise.resolve(config);
+    }
+
+    if (config.data instanceof URLSearchParams) {
         config.headers.set({
             "Content-Type": "application/x-www-form-urlencoded"
         });
+
+        config.data = config.data.toString();
+        return Promise.resolve(config);
+    }
+
+    if (isString(config.data)) {
+        config.headers.set({
+            "Content-Type": "text/plain"
+        });
+    }
+
+    if (isObjectLike(config.data)) {
+        const contentType = config.headers.get("Content-Type");
+
+        if (contentType && contentType.includes("application/x-www-form-urlencoded")) {
+            config.data = convertToEncodedForm(config.data);
+        } else {
+            config.headers.set({
+                "Content-Type": "application/json"
+            });
+
+            config.data = safeStringify(config.data);
+        }
     }
 
     return Promise.resolve(config);
@@ -29,26 +100,24 @@ export const decodeResponseData = new PipelineExecutor<ResponseExecutor>(async (
 
     const encoding = response.headers["content-encoding"];
 
-    if (Buffer.isBuffer(response.data) && response.data.length) {
-        if (encoding) {
-            const decoder = DECODERS[encoding];
+    if (isBuffer(response.data) && response.data.length) {
+        const decoder = DECODERS[encoding];
 
-            if (decoder) {
-                const decompressed = await decoder(response.data);
+        if (encoding && isFunction(decoder)) {
+            const decompressed = await decoder(response.data);
 
-                if (maxContentLength > -1 || maxContentLength !== Infinity) {
-                    if (decompressed.length > maxContentLength) {
-                        throw new Error(
-                            `Content length of ${decompressed.length} exceeds the maxContentLength of ${maxContentLength}`
-                        );
-                    }
+            if (maxContentLength > -1 || maxContentLength !== Infinity) {
+                if (decompressed.length > maxContentLength) {
+                    throw new Error(
+                        `Content length of ${decompressed.length} exceeds the maxContentLength of ${maxContentLength}`
+                    );
                 }
-
-                response.data = decompressed;
             }
+
+            response.data = decompressed;
         }
     } else {
-        response.data = null;
+        response.data = undefined;
     }
 
     return Promise.resolve(response);
